0d4620d8a7ebd7fcbbadd58ff29b243a06daa3ba
Michal Olszewski <olszewski.m.l@gmail.com>
2017-05-19 01:22:12 +0200

    Fixed usage of nonmodular framework header

--- CoreData-Views-Collection-ios/Classes/CoreDataGanttViewController.h
+++ CoreData-Views-Collection-ios/Classes/CoreDataGanttViewController.h
@@ -6,8 +6,8 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
-#import "GbGanttDataSource.h"
-#import "GbGanttDelegate.h"
+#import <Gb-Gantt/GbGanttDataSource.h>
+#import <Gb-Gantt/GbGanttDelegate.h>
 
 
 @interface CoreDataGanttViewController : UIViewController <GbGanttDataSource, NSFetchedResultsControllerDelegate>

cc9dd3a2824f83907a9234f4b72156406cfe7227
Michal Olszewski <olszewski.m.l@gmail.com>
2017-06-30 12:15:25 +0200

    Reverted project settings modifications

--- CoreData-Views-Collection-ios/Classes/CoreDataGanttViewController.h
+++ CoreData-Views-Collection-ios/Classes/CoreDataGanttViewController.h
@@ -6,7 +6,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
-@import GbGantt;
+#import "GbGanttDataSource.h"
 
 
 @interface CoreDataGanttViewController : UIViewController <GbGanttDataSource, NSFetchedResultsControllerDelegate>
